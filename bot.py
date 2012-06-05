b59ac96f2717ff33aefddcc797b384208eb3e10c
Nijaxor <Nijaxor@hushmail.com>
2012-06-05 00:27:01 +0000

    Fixed stuff...

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ ircChannel = "#kill"
 ircKillReason = "Killed!"
 ircCommandPrefix = "!"
 ircHopDelay = 240 #seconds
+ircUnBanDelay = 240 #seconds
 ircIgnoreModes = ["o", "I"] #ignore users with these modes
 ircOperUser = "KillServ"
 ircOperPassword = "password"
@@ -38,11 +39,16 @@ def writer(text):
 	print text
 	
 #used to hop after set delay
-def hopAfterDelay(nick):
+def hopAfterDelay(a):
 	time.sleep(ircHopDelay)
-	writer("MODE " + ircChannel + " +h " + nick)
-	writer("PRIVMSG " + ircChannel + " :User " + nick + " can now kill users with no level but cannot be killed!")
-	 
+	if a[1] == True:
+		writer("MODE " + ircChannel + " +h " + a[0])
+		writer("PRIVMSG " + ircChannel + " :User " + a[0] + " can now kill users that don't have a level but cannot be killed!")
+
+#used to unban after set dely
+def unBanAfterDelay(nick):
+	time.sleep(ircUnBanDelay0
+	writer("MODE " + ircChannel + " -b " + a)
 #get nickname from PRIVMSG
 def getNick(line):
 	linesplit = line.split()
@@ -67,53 +73,83 @@ while 1:
 	if len(line) > 0:
 		print line.strip()
 		linesplit = line.split()
+		#ping/pong
 		if linesplit[0] == "PING":
 			writer("PONG " + linesplit[1])
 		if len(linesplit) > 1:
+			#on connect
 			if linesplit[1] == "001":
 				writer("PRIVMSG nickserv :identify " + ircPassword)
 				writer("OPER " + ircOperUser + " " + ircOperPassword)
 				writer("JOIN " + ircChannel)
+			#channel users
 			if linesplit[1] == "353":
 				linesplit[5] = linesplit[5].replace(":", "", 1)
 				for user in linesplit[5:]:
 					status = ""
-					if (user.startswith("+")) or (user.startswith("%")) or (user.startswith("@")) or (user.startswith("&")) or (user.startswith("~")):
-						status = user[:1]
-						user = user.replace(user[:1], "", 1)
+					if user.startswith("+"):
+						status = status + "v"
+					if user.startswith("%"):
+						status = status + "h"
+					if user.startswith("@"):
+						status = status + "o"
+					if user.startswith("&"):
+						status = status + "ao"
+					if user.startswith("~"):
+						status = status + "q"
+					user = user.replace(user[:1], "", 1).lower()
 					channelUsers[user] = status
+				print channelUsers
+			#process joins
 			if linesplit[1] == "JOIN":
 				if linesplit[2].replace(":", "", 1) == ircChannel:
 					nick = getNick(line).lower()
-					channelUsers[nick] = ""
-					writer("WHOIS " + nick)
+					if not nick == ircNick:
+						channelUsers[nick] = ""
+						writer("WHOIS " + nick)
+			#process parts
 			if linesplit[1] == "PART":
 				if linesplit[2].replace(":", "", 1) == ircChannel:
 					nick = getNick(line).lower()
 					if nick in channelUsers:
 						del channelUsers[nick]
 					if nick in hopTimers:
-						hopTimers[nick]._Thread__stop()
+						hopTimers[nick][1] = False
+			#process quits
 			if linesplit[1] == "QUIT":
 				nick = getNick(line).lower()
 				if nick in channelUsers:
 					del channelUsers[nick]
 				if nick in hopTimers:
-					hopTimers[nick]._Thread__stop()
+					hopTimers[nick][1] = False
+			#process nick changes
+			if linesplit[1] == "NICK":
+				nick = getNick(line).lower()
+				newNick = linesplit[2].replace(":", "", 1)
+				if nick in channelUsers:
+					channelUsers[newNick.lower()] = channelUsers[nick]
+					del channelUsers[nick]
+					if nick.lower() in hopTimers:
+						hopTimers[newNick.lower()] = hopTimers[nick.lower()]
+						hopTimers[newNick.lower()][0] = newNick
+						del hopTimers[nick.lower()]
+			#looking for ignore modes
 			if linesplit[1] == "379":
 				ignore = False
 				for mode in ircIgnoreModes:
 					if mode in linesplit[7]:
 						ignore = True
 				if ignore == False:
-					newThread = threading.Thread(target=hopAfterDelay, args=(linesplit[3],))
-					hopTimers[linesplit[3].lower()] = newThread
-					hopTimers[linesplit[3].lower()].start()
-					writer("NOTICE " + linesplit[3] + " :Please wait " + str(ircHopDelay) + " seconds until you can't be killed and can kill users without a level!")
+					hopTimers[linesplit[3].lower()] = [linesplit[3], True]
+					newThread = threading.Thread(target=hopAfterDelay, args=(hopTimers[linesplit[3].lower()],))
+					newThread.start()
+					writer("NOTICE " + linesplit[3] + " :Please wait " + str(ircHopDelay) + " seconds before you can't be killed and can kill users that don't have a level!")
 				else:
 					writer("MODE " + ircChannel + " +h " + linesplit[3])
+			#handle change in modes
 			if linesplit[1] == "MODE":
 				if linesplit[2] == ircChannel:
+					normalNick = linesplit[4]
 					linesplit[4] = linesplit[4].lower()
 					if linesplit[3].startswith("+"):
 						if "v" in linesplit[3]:
@@ -125,9 +161,9 @@ while 1:
 						if "a" in linesplit[3]:
 							channelUsers[linesplit[4]] = channelUsers[linesplit[4]] + "a"
 						if "q" in linesplit[3]:
-							channelUsers[linesplit[4]] = channelUsers[linesplit[4]] + "q"
+							channelUsers[linesplit[4]] = channelUsers[linesplit[4]] + "1"
 						if linesplit[4] in hopTimers:
-							hopTimers[linesplit[4]]._Thread__stop()
+							hopTimers[linesplit[4]][1] = False
 					if linesplit[3].startswith("-"):
 						if "v" in linesplit[3]:
 							channelUsers[linesplit[4]] = channelUsers[linesplit[4]].replace("v", "")
@@ -139,6 +175,12 @@ while 1:
 							channelUsers[linesplit[4]] = channelUsers[linesplit[4]].replace("a", "")
 						if "q" in linesplit[3]:
 							channelUsers[linesplit[4]] = channelUsers[linesplit[4]].replace("q", "")
+						if len(channelUsers[linesplit[4]]) == 0:
+							hopTimers[linesplit[4]] = [normalNick, True]
+							newThread = threading.Thread(target=hopAfterDelay, args=(hopTimers[linesplit[4]],))
+							newThread.start()
+							writer("NOTICE " + linesplit[4] + " :Please wait " + str(ircHopDelay) + " seconds before you can't be killed and can kill users that don't have a level!")
+			#process privmsgs
 			if linesplit[1] == "PRIVMSG":
 				nick = getNick(line).lower()
 				if linesplit[3] == ":" + ircCommandPrefix + "kill":
@@ -146,6 +188,8 @@ while 1:
 						if len(channelUsers[nick]) > 0:
 							if linesplit[4].lower() in channelUsers:
 								if len(channelUsers[linesplit[4].lower()]) == 0:
+									writer("MODE " + ircChannel + " +b " + linesplit[4])
+									unBanAfterDelay(linesplit[4])
 									writer("KILL " + linesplit[4] + " :" + ircKillReason)
 								else:
 									writer("NOTICE " + nick + " :You cannot kill that user.")
